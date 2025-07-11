0e20764beb30afd9995b95f79525ffb0ae3f2e03
cris1059 <149038713+cris1059@users.noreply.github.com>
2025-07-11 16:46:28 -0600

    Actualizacion de los videos

--- frontend/src/templates/Servicios.jsx
+++ frontend/src/templates/Servicios.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import '../assets/css/servicios.css';
@@ -11,6 +11,41 @@ import Curs from "../assets/img/catalogo/cursos.jpg";
 
 function Servicios() {
   const navigate = useNavigate();
+
+  
+useEffect(() => {
+  // Cargar API de YouTube solo una vez
+  const tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/iframe_api';
+  document.body.appendChild(tag);
+
+  let player;
+
+  // Esta función se ejecutará cuando la API de YouTube esté lista
+  window.onYouTubeIframeAPIReady = function () {
+    player = new window.YT.Player('videoFrame');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          player.playVideo();
+        } else {
+          player.pauseVideo();
+        }
+      });
+    }, {
+      threshold: 0.5, // Al menos 50% visible
+    });
+
+    const iframe = document.getElementById('videoFrame');
+    if (iframe) observer.observe(iframe);
+  };
+
+  return () => {
+    // Limpieza opcional
+    delete window.onYouTubeIframeAPIReady;
+  };
+}, []);
 
   const items = [
     {
@@ -82,7 +117,28 @@ link: "/cursos"
 
   return (
     <section id="servicios">
-      <h2 data-aos="zoom-in">Impulsa tu negocio con nuestros servicios especializados</h2>
+      <h2 data-aos="zoom-in">Servicios</h2>
+      <div data-aos="zoom-in" className='flex curso-activo'>
+        <div className="video">
+                  <iframe
+          id="videoFrame"
+          width="310"
+          height="310"
+          src="https://www.youtube.com/embed/g3qkp0GgUFo?enablejsapi=1&mute=1"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+        </div>
+        <div className='titlevideo'>
+          <h2 >¿Te interesa este curso?</h2>
+          <p>Inscríbete a nuestro curso especializado y recibe asesoría personalizada con expertos en normativas sanitarias. Aprenderás a identificar los requisitos legales que aplican a tu negocio, evitar sanciones y garantizar la operación segura y legal de tus productos o servicios.</p>
+
+        </div>
+      </div>
+      
+        <h2 data-aos="zoom-in">Servicios especializados</h2>
       <div data-aos="zoom-in" className='content_catalogo'>
 
       <div className='iconosS'>
@@ -118,7 +174,7 @@ link: "/cursos"
       </div>
       </div>
 
-</div>
+      </div>
     </section>
   );
 }
